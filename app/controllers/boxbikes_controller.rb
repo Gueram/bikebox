4d7e756204a7a7f66c1c0a71ef31162d03f2a9fa
Aram Sulukdjian <asulukdjian@pm.me>
2020-11-17 17:23:18 +0100

    boxbikes controller new and create done and new view done

--- app/controllers/boxbikes_controller.rb
+++ app/controllers/boxbikes_controller.rb
@@ -4,11 +4,21 @@ class BoxbikesController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
     @boxbike = Boxbike.new
   end
 
   def create
-    @user = User.find(params[:user_id])
+    @boxbike = Boxbike.new(boxbike_params)
+    if @boxbike.save
+      redirect_to boxbike_path(@boxbike)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def boxbike_params
+    params.require(:boxbike).permit(:address, :capacity, :price, :charge_option)
   end
 end
