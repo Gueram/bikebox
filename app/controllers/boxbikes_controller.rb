6815a3fbe178a1438d59b337769e7f2bce16c2cf
Aram Sulukdjian <asulukdjian@pm.me>
2020-11-17 16:49:18 +0100

    started working on boxbikes controller and new view

--- app/controllers/boxbikes_controller.rb
+++ app/controllers/boxbikes_controller.rb
@@ -2,4 +2,13 @@ class BoxbikesController < ApplicationController
   def index
     @boxbikes = Boxbike.all
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @boxbike = Boxbike.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+  end
 end
