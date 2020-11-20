ec0ac9b56347c7796d3621a043617dc49e016311
Gueram Minassian <gueram.minassian@gmail.com>
2020-11-20 01:27:37 +0100

    Show_front_en_cours

--- app/javascript/plugins/price.js
+++ app/javascript/plugins/price.js
@@ -4,17 +4,17 @@ const roundAmount = (amount) => {
 
 const dynamicPrice = (startDate, endDate, totalBikesString) => {
   const totalDays = document.getElementById("total-days");
-  const boxPricePerDay = document.getElementById("box-price-per-day");
+  const boxPricePerDay = document.getElementById("box-price-per-day").innerText;
   const totalPriceElement = document.getElementById("total-price");
   let dateDiffInMilliseconds = new Date(endDate.value) - new Date(startDate.value);
   let nbrOfDays = dateDiffInMilliseconds / 86400000;
-  let boxPricePerDayReduced = boxPricePerDay.innerText - nbrOfDays / 100;
+  /* let boxPricePerDayReduced = boxPricePerDay.innerText - nbrOfDays / 10; */
   const totalBikesInt = parseInt(totalBikesString.value, 10);
   let totalBikes = isNaN(totalBikesInt) ? 1 : totalBikesInt
   if (startDate.value && endDate.value) {
     totalDays.innerText = nbrOfDays
-    boxPricePerDay.innerText = roundAmount(boxPricePerDayReduced)
-    const totalPrice = nbrOfDays * boxPricePerDayReduced * totalBikes
+    /* boxPricePerDay.innerText = roundAmount(boxPricePerDayReduced) */
+    const totalPrice = nbrOfDays * boxPricePerDay * totalBikes
     totalPriceElement.innerText = roundAmount(totalPrice)
   }
 };
